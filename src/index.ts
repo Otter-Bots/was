d1196d0b75af4c5628793d2116b1bd6b4a54dca9
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-11-15 21:00:39 +0000

    Get the core finally working

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
 import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import fetch from "node-fetch";
 const app = express()
-
-// respond with "hello world" when a GET request is made to the homepage
+app.use(cors())
 app.get('/', (_req, res) => {
   res.send('Authentication Service for Otter Bots')
+})
+// Discord BS
+app.get('/discord/', (_req, res) => {
+  res.sendFile('discord-callback.html', { root: `${process.cwd()}/html/` })
+})
+app.get('/discord/finishAuth', (req, res) => {
+  const { access_token, token_type} = req.query;
+  // res.send(`Access Token: ${access_token} Token Type: ${token_type}`)
+  fetch('https://discord.com/api/users/@me', {
+			headers: {
+				authorization: `${token_type} ${access_token}`,
+			},
+		})
+			.then(result => result.json())
+			.then(response => {
+				const { username, discriminator } = response;
+        console.log(`Authed as ${username}#${discriminator}`)
+        res.send(`Authed as ${username}#${discriminator}`)
+			})
+			.catch(console.error);
+})
+app.listen(process.env.PORT, () => {
+  console.log(`app listening on port ${process.env.PORT}!`)
 })
